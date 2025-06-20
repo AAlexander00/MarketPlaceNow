738f93808d8b11cc1751750b1dfa68de8ff17d94
AAlexander00 <luisalejandromunozguarnizo@gmail.com>
2025-06-19 22:31:16 -0500

    Actualizacion detalle de productos

--- Marketplacenow/productos/urls.py
+++ Marketplacenow/productos/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('busqueda/', views.busqueda, name='busqueda'),
     path('productos/', views.lista_productos, name='lista_productos'),
+    path('', views.lista_productos, name='inicio'),
     path('<int:producto_id>/', views.detalle_producto, name='detalle_producto'),
 ]
