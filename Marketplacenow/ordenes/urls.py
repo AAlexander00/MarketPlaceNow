072c3b74ab37f1128e1f84a293fad0c3b7b00e4e
AAlexander00 <luisalejandromunozguarnizo@gmail.com>
2025-06-25 13:06:03 -0500

    Actualizacion ordenes

--- Marketplacenow/ordenes/urls.py
+++ Marketplacenow/ordenes/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('orden/<int:orden_id>/', views.detalle_orden, name='detalle_orden'),
     path('orden/<int:orden_id>/pagar/', views.pagar_orden, name='pagar_orden'),
+    path('ordenar/<int:producto_id>/', views.ordenar_producto, name='ordenar_producto'),
 ]
--- Marketplacenow/ordenes/views.py
+++ Marketplacenow/ordenes/views.py
@@ -3,6 +3,8 @@ from ordenes.models import Orden
 from productos.models import Producto
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
+from .models import Orden, DetalleOrden
+from django.contrib import messages
 
 @login_required
 def detalle_orden(request, orden_id):
@@ -20,3 +22,25 @@ def pagar_orden(request, orden_id):
         return redirect('perfil')  # Redirige a perfil u otra vista de confirmación
     
     return redirect('detalle_orden', orden_id=orden.id)
+
+@login_required
+def ordenar_producto(request, producto_id):
+    producto = get_object_or_404(Producto, pk=producto_id)
+
+    # Crear la orden
+    orden = Orden.objects.create(
+        usuario=request.user,
+        total=producto.PRECIO,
+        estado='pagado'
+    )
+
+    # Crear detalle de la orden
+    DetalleOrden.objects.create(
+        orden=orden,
+        producto=producto,
+        cantidad=1,
+        precio_unitario=producto.PRECIO
+    )
+
+    messages.success(request, 'Compra realizada con éxito.')
+    return redirect('detalle_orden', orden.id)
